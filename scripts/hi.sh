7096536e49da0deda6ccc15f68d67e8a3df3c9f7
blueswen <blueswen.tw@gmail.com>
2023-07-02 14:28:48 +0800

    Add Hindi

---
+++ scripts/hi.sh
@@ -0,0 +1,35 @@
+curl \
+-X POST \
+-H "Content-Type: application/json" \
+-o sample-hi.pdf \
+-d '{
+    "template": "sample-hi.jasper",
+    "fileName": "sample-hi",
+    "parameters": {
+    "store_number": "5566"
+    },
+    "data": [
+    {
+        "ind": "1",
+        "name": "सेब",
+        "amount": "3",
+        "price": "30",
+        "total_price": "90"
+    },
+    {
+        "ind": "2",
+        "name": "संतरा",
+        "amount": "2",
+        "price": "40",
+        "total_price": "80"
+    },
+    {
+        "ind": "3",
+        "name": "तरबूज",
+        "amount": "1",
+        "price": "100",
+        "total_price": "100"
+    }
+    ]
+}' \
+http://localhost:8080/report
